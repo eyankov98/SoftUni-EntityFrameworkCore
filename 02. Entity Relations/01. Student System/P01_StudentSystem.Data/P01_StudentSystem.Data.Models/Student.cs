dab9ee5fe75409597f57255615df4071ed508257
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-10 22:27:05 +0200

    Add files via upload

---
+++ 02. Entity Relations/01. Student System/P01_StudentSystem.Data/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            Homeworks = new HashSet<Homework>();
+            StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTime RegisteredOn { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public ICollection<Homework> Homeworks { get; set; }
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+    }
+}
