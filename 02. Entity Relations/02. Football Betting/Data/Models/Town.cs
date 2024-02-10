6c7a4ac937cebe4474873bdbb1c6c0e41349303c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-10 22:44:53 +0200

    Add files via upload

---
+++ 02. Entity Relations/02. Football Betting/Data/Models/Town.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Town
+{
+    public Town()
+    {
+        Teams = new HashSet<Team>();
+    }
+
+    [Key]
+    public int TownId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+
+    public int CountryId { get; set; }
+
+    [ForeignKey(nameof(CountryId))]
+    public virtual Country Country { get; set; }
+
+
+    public virtual ICollection<Team> Teams { get; set; }
+}
