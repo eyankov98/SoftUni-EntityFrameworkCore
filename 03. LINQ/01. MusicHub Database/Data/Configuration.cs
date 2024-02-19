3a7f26a3fb84f295beafa3f48334ab660e53c96e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-19 21:18:04 +0200

    Add files via upload

---
+++ 03. LINQ/01. MusicHub Database/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-I00ATHT;Database=MusicHub;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
