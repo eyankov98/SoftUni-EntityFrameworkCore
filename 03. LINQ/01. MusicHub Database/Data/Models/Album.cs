62dd17759507f46e2507cccd0bc80a4635943d09
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-19 21:15:08 +0200

    Add files via upload

---
+++ 03. LINQ/01. MusicHub Database/Data/Models/Album.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required] 
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => Songs.Sum(s => s.Price);
+
+        public int? ProducerId { get; set; }
+        [ForeignKey(nameof(ProducerId))]
+        public virtual Producer? Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
---
+++ 03. LINQ/01. MusicHub Database/Data/Models/SongPerformer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        public int SongId { get; set; }
+        [ForeignKey(nameof(SongId))]
+        [Required]
+        public virtual Song Song { get; set; }
+
+        public int PerformerId { get; set; }
+        [ForeignKey(nameof(PerformerId))]
+        [Required]
+        public virtual Performer Performer { get; set;}
+    }
+}
