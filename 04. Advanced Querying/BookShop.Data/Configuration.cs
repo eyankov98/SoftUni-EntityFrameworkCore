944a4e9237180c579370582b3b24f4657174e182
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-24 22:02:34 +0200

    Add files via upload

---
+++ 04. Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-I00ATHT;Database=BookShop;Integrated Security=True;";
+    }
+}
