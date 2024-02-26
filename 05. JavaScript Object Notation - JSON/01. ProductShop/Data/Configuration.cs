d3cee1f0008c24e8066206aea7bf933e31de5566
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-26 22:21:14 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/01. ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-I00ATHT;Database=ProductShop;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
