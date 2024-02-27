6f3609a13e3b6f51bf56e51d6fab4d999333f240
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-27 22:47:58 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/02. CarDealer/DTOs/Import/CarDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    public class CarDTO
+    {
+        public CarDTO()
+        {
+            PartsId = new HashSet<int>();
+        }
+
+        public string Make { get; set; } = null!;
+
+        public string Model { get; set; } = null!;
+
+        public long TraveledDistance { get; set; }
+
+        public virtual ICollection<int> PartsId { get; set; }
+    }
+}
