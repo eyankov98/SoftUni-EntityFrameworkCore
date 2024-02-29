30debb417d2d5d6191817b46de33bbc9eb28c005
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-29 23:09:52 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/01. ProductShop/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-I00ATHT;Database=ProductShop;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+    }
+}
