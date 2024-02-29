88dd24f056571c9a926d14498afbed54ce256ace
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-02-29 23:13:44 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/01. ProductShop/Utilities/XmlParser.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Utilities
+{
+    public class XmlParser
+    {
+        public T[] DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRootAttribute);
+
+            using StringReader stringReader = new StringReader(inputXml);
+            T[] deserializedObj = (T[])xmlSerializer.Deserialize(stringReader);
+
+            return deserializedObj;
+        }
+
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            using StringReader stringReader = new StringReader(inputXml);
+            T deserializedObj = (T)xmlSerializer.Deserialize(stringReader);
+
+            return deserializedObj;
+        }
+
+        public string SerializeCollection<T>(T[] obj, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRootAttribute);
+
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            using StringWriter stringWriter = new StringWriter(stringBuilder);
+
+            xmlSerializer.Serialize(stringWriter, obj, xmlSerializerNamespaces);
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public string Serialize<T>(T obj, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute= new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            using StringWriter stringWriter = new StringWriter(stringBuilder);
+
+            xmlSerializer.Serialize(stringWriter, obj, xmlSerializerNamespaces);
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
