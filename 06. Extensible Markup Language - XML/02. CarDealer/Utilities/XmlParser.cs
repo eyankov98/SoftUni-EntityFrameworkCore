c487f47ecb436cb42098300ccac9e6829bf72db4
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-01 23:51:31 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/02. CarDealer/Utilities/XmlParser.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.Utilities
+{
+    public class XmlParser
+    {
+        public T[] DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRootAttribute);
+
+            using StringReader sr = new StringReader(inputXml);
+            T[] deserializedObj = (T[])xmlSerializer.Deserialize(sr);
+
+            return deserializedObj;
+        }
+
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            using StringReader sr = new StringReader(inputXml);
+            T deserializedObj = (T)xmlSerializer.Deserialize(sr);
+
+            return deserializedObj;
+        }
+
+        public string SerializeCollection<T>(T[] obj, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRootAttribute);
+
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(sw, obj, xmlSerializerNamespaces);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Serialize<T>(T obj, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(sw , obj, xmlSerializerNamespaces);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
