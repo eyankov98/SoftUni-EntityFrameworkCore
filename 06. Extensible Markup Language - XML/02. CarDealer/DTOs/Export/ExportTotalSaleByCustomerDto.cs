1b981e2739fdde2607de0d1d29ff87fcc30721dc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-01 23:49:39 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/02. CarDealer/DTOs/Export/ExportTotalSaleByCustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("customer")]
+    public class ExportTotalSaleByCustomerDto
+    {
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; } = null!;
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public string SpentMoney { get; set; }
+    }
+}
