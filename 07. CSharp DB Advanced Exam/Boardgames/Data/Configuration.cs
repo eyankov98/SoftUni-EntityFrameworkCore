cdaacd93100f6f3b29934556328768cfeacbf88e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:45:54 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-I00ATHT;Database=Boardgames;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+    }
+}
