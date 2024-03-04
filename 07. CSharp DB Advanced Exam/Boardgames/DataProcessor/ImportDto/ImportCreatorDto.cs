086cdf08b8dff980c1f286bfba7d319a19d13bbc
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:47:44 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/DataProcessor/ImportDto/ImportCreatorDto.cs
@@ -0,0 +1,54 @@
+﻿using Boardgames.Shared;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Creator")]
+    public class ImportCreatorDto
+    {
+        [XmlElement("FirstName")]
+        [Required]
+        [MaxLength(GlobalConstants.CreatorFirstNameMaxLength)]
+        [MinLength(GlobalConstants.CreatorFirstNameMinLength)]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("LastName")]
+        [Required]
+        [MaxLength(GlobalConstants.CreatorLastNameMaxLength)]
+        [MinLength(GlobalConstants.CreatorLastNameMinLength)]
+        public string LastName { get; set; } = null!;
+
+        [XmlArray("Boardgames")]
+        public ImportBoardgameDto[] Boardgames { get; set; } = null!;
+    }
+
+    [XmlType("Boardgame")]
+    public class ImportBoardgameDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MaxLength(GlobalConstants.BoardgameNameMaxLength)]
+        [MinLength(GlobalConstants.BoardgameNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Rating")]
+        [Required]
+        [Range(GlobalConstants.BoardgameRatingMinRange, GlobalConstants.BoardgameRatingMaxRange)]
+        public double Rating { get; set; }
+
+        [XmlElement("YearPublished")]
+        [Required]
+        [Range(GlobalConstants.BoardgameYearPublishedMinRange, GlobalConstants.BoardgameYearPublishedMaxRange)]
+        public int YearPublished { get; set; }
+
+        [XmlElement("CategoryType")]
+        [Required]
+        [Range(GlobalConstants.BoardgameCategoryTypeMinRange, GlobalConstants.BoardgameCategoryTypeMaxRange)]
+        public int CategoryType { get; set; }
+
+        [XmlElement("Mechanics")]
+        [Required]
+        public string Mechanics { get; set; } = null!;
+    }
+}
---
+++ 07. CSharp DB Advanced Exam/Boardgames/DataProcessor/ImportDto/ImportSellerDto.cs
@@ -0,0 +1,33 @@
+﻿using Boardgames.Shared;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    public class ImportSellerDto
+    {
+        [JsonProperty("Name")]
+        [Required]
+        [MaxLength(GlobalConstants.SellerNameMaxLength)]
+        [MinLength(GlobalConstants.SellerNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Address")]
+        [Required]
+        [MaxLength(GlobalConstants.SellerAddressMaxLength)]
+        [MinLength(GlobalConstants.SellerAddressMinLength)]
+        public string Address { get; set; } = null!;
+
+        [JsonProperty("Country")]
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [JsonProperty("Website")]
+        [Required]
+        [RegularExpression(GlobalConstants.SellerWebsiteRegex)]
+        public string Website { get; set; } = null!;
+
+        [JsonProperty("Boardgames")]
+        public int[] BoardgamesIds { get; set; } = null!;
+    }
+}
