a5fb0b3f737afcd2b9e1051dcb5fa827a6d6705c
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:48:15 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/DataProcessor/Deserializer.cs
@@ -0,0 +1,143 @@
+﻿namespace Boardgames.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using Boardgames.Data;
+    using Boardgames.Data.Models;
+    using Boardgames.Data.Models.Enums;
+    using Boardgames.DataProcessor.ImportDto;
+    using Boardgames.Utilities;
+    using Newtonsoft.Json;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCreator
+            = "Successfully imported creator – {0} {1} with {2} boardgames.";
+
+        private const string SuccessfullyImportedSeller
+            = "Successfully imported seller - {0} with {1} boardgames.";
+
+        public static string ImportCreators(BoardgamesContext context, string xmlString)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportCreatorDto[] creatorDtos = xmlHelper.Deserialize<ImportCreatorDto[]>(xmlString, "Creators");
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            HashSet<Creator> validCreators = new HashSet<Creator>();
+
+            foreach (var creatorDto in creatorDtos)
+            {
+                if (!IsValid(creatorDto))
+                {
+                    stringBuilder.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Creator creator = new Creator()
+                {
+                    FirstName = creatorDto.FirstName,
+                    LastName = creatorDto.LastName
+                };
+
+                foreach (var boardgameDto in creatorDto.Boardgames)
+                {
+                    if (!IsValid(boardgameDto) || string.IsNullOrEmpty(boardgameDto.Name))
+                    {
+                        stringBuilder.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Boardgame boardgame = new Boardgame()
+                    {
+                        Name = boardgameDto.Name,
+                        Rating = boardgameDto.Rating,
+                        YearPublished = boardgameDto.YearPublished,
+                        CategoryType = (CategoryType)boardgameDto.CategoryType,
+                        Mechanics = boardgameDto.Mechanics
+                    };
+
+                    creator.Boardgames.Add(boardgame);
+                }
+
+                validCreators.Add(creator);
+
+                stringBuilder.AppendLine(string.Format(SuccessfullyImportedCreator, creator.FirstName, creator.LastName, creator.Boardgames.Count));
+            }
+
+            context.Creators.AddRange(validCreators);
+            context.SaveChanges();
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public static string ImportSellers(BoardgamesContext context, string jsonString)
+        {
+            ImportSellerDto[] sellerDtos = JsonConvert.DeserializeObject<ImportSellerDto[]>(jsonString);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            HashSet<Seller> validSellers = new HashSet<Seller>();
+
+            int[] boardgamesIds = context.Boardgames
+                .Select(b => b.Id)
+                .ToArray();
+
+            foreach (var sellerDto in sellerDtos)
+            {
+                if (!IsValid(sellerDto)
+                    || string.IsNullOrEmpty(sellerDto.Country)
+                    || string.IsNullOrEmpty(sellerDto.Website)
+                    || string.IsNullOrEmpty(sellerDto.Address))
+                {
+                    stringBuilder.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Seller seller = new Seller()
+                {
+                    Name = sellerDto.Name,
+                    Address = sellerDto.Address,
+                    Country = sellerDto.Country,
+                    Website = sellerDto.Website
+                };
+
+                foreach (var boardgameId in sellerDto.BoardgamesIds.Distinct())
+                {
+                    if (!boardgamesIds.Contains(boardgameId))
+                    {
+                        stringBuilder.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    BoardgameSeller boardgame = new BoardgameSeller()
+                    {
+                        BoardgameId = boardgameId
+                    };
+
+                    seller.BoardgamesSellers.Add(boardgame);
+                }
+
+                validSellers.Add(seller);
+
+                stringBuilder.AppendLine(string.Format(SuccessfullyImportedSeller, seller.Name, seller.BoardgamesSellers.Count));
+            }
+
+            context.Sellers.AddRange(validSellers);
+            context.SaveChanges();
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ 07. CSharp DB Advanced Exam/Boardgames/DataProcessor/Serializer.cs
@@ -0,0 +1,65 @@
+﻿namespace Boardgames.DataProcessor
+{
+    using Boardgames.Data;
+    using Boardgames.DataProcessor.ExportDto;
+    using Boardgames.Utilities;
+    using Newtonsoft.Json;
+
+    public class Serializer
+    {
+        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
+        {
+            var creators = context.Creators
+                .Where(c => c.Boardgames.Any())
+                .Select(c => new ExportCreatorDto()
+                {
+                    BoardgamesCount = c.Boardgames.Count,
+                    CreatorName = c.FirstName + " " + c.LastName,
+                    Boardgames = c.Boardgames
+                        .Select(b => new ExportBoardgameDto()
+                        {
+                            BoardgameName = b.Name,
+                            BoardgameYearPublished = b.YearPublished
+                        })
+                        .OrderBy(b => b.BoardgameName)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.BoardgamesCount)
+                    .ThenBy(c => c.CreatorName)
+                .ToArray();
+
+            XmlHelper xmlHelper = new XmlHelper();
+
+            return xmlHelper.Serialize(creators, "Creators");
+        }
+
+        public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
+        {
+            var sellers = context.Sellers
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .Select(s => new
+                {
+                    Name = s.Name,
+                    Website = s.Website,
+                    Boardgames = s.BoardgamesSellers
+                        .Where(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating)
+                        .Select(bs => new
+                        {
+                            Name = bs.Boardgame.Name,
+                            Rating = bs.Boardgame.Rating,
+                            Mechanics = bs.Boardgame.Mechanics,
+                            Category = bs.Boardgame.CategoryType.ToString()
+                        })
+                        .OrderByDescending(bs => bs.Rating)
+                            .ThenBy(bs => bs.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(s => s.Boardgames.Count())
+                    .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(sellers, Formatting.Indented);
+        }
+    }
+}
