34bc7fdd84caf843138ede037abf71b7d17f065e
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:50:26 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/Shared/GlobalConstants.cs
@@ -0,0 +1,34 @@
+﻿namespace Boardgames.Shared
+{
+    public class GlobalConstants
+    {
+        // Boardgame
+        public const int BoardgameNameMinLength = 10;
+        public const int BoardgameNameMaxLength = 20;
+
+        public const double BoardgameRatingMinRange = 1.00;
+        public const double BoardgameRatingMaxRange = 10.00;
+
+        public const int BoardgameYearPublishedMinRange = 2018;
+        public const int BoardgameYearPublishedMaxRange = 2023;
+
+        public const int BoardgameCategoryTypeMinRange = 0;
+        public const int BoardgameCategoryTypeMaxRange = 4;
+
+        // Seller
+        public const int SellerNameMinLength = 5;
+        public const int SellerNameMaxLength = 20;
+
+        public const int SellerAddressMinLength = 2;
+        public const int SellerAddressMaxLength = 30;
+
+        public const string SellerWebsiteRegex = @"www.[A-Za-z0-9\-]{3,}.com";
+
+        // Creator
+        public const int CreatorFirstNameMinLength = 2;
+        public const int CreatorFirstNameMaxLength = 7;
+
+        public const int CreatorLastNameMinLength = 2;
+        public const int CreatorLastNameMaxLength = 7;
+    }
+}
