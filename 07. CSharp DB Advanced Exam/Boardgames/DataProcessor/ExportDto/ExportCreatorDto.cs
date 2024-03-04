dc104799def2e80be2fc58b9386dfbf19be38ac7
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:46:59 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/DataProcessor/ExportDto/ExportCreatorDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    [XmlType("Creator")]
+    public class ExportCreatorDto
+    {
+        [XmlAttribute("BoardgamesCount")]
+        public int BoardgamesCount { get; set; }
+
+        [XmlElement("CreatorName")]
+        public string CreatorName { get; set; } = null!;
+
+        [XmlArray("Boardgames")]
+        public ExportBoardgameDto[] Boardgames { get; set; } = null!;
+    }
+
+    [XmlType("Boardgame")]
+    public class ExportBoardgameDto
+    {
+        [XmlElement("BoardgameName")]
+        public string BoardgameName { get; set; } = null!;
+
+        [XmlElement("BoardgameYearPublished")]
+        public int BoardgameYearPublished { get; set; }
+    }
+}
