18ba5f02f6f5b1bb279977606bb5ad6dd0be1cd0
Edgar Yankov <122820733+eyankov98@users.noreply.github.com>
2024-03-04 18:50:52 +0200

    Add files via upload

---
+++ 07. CSharp DB Advanced Exam/Boardgames/Utilities/XmlHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace Boardgames.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            T deserializedDtos = (T)xmlSerializer.Deserialize(stringReader);
+
+            return deserializedDtos;
+        }
+
+        public string Serialize<T>(T obj, string rootName)
+        {
+            XmlRootAttribute xmlRootAttribute = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRootAttribute);
+
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add(string.Empty, string.Empty);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            using StringWriter stringWriter = new StringWriter(stringBuilder);
+
+            xmlSerializer.Serialize(stringWriter, obj, xmlSerializerNamespaces);
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
